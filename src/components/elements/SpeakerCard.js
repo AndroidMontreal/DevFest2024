e18078e511d62d4dddfe8294aceabae08a146f0c
Jhin Lee <leehack@gmail.com>
2024-09-03 10:26:56 -0400

    Add 2023 speaker links

--- src/components/elements/SpeakerCard.js
+++ src/components/elements/SpeakerCard.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const SpeakerCard = ({ speaker }) => {
   return (
-    <div className="flex flex-col items-center justify-center p-4 border-2 border-gray-200 rounded-lg w-80 h-80">
+    <a href={speaker.link} target="_blank" rel="noopener noreferrer" className="flex flex-col items-center justify-center p-4 border-2 border-gray-200 rounded-lg w-80 h-80 hover:shadow-lg transition-shadow duration-300">
       <Image
         className="rounded-full w-40 h-40 object-cover"
         src={speaker.image}
@@ -10,11 +10,11 @@ const SpeakerCard = ({ speaker }) => {
         width={96}
         height={96}
       />
-      <div className="mt-4">
+      <div className="mt-4 text-center">
         <h3 className="text-lg font-bold">{speaker.name}</h3>
         <p className="text-sm">{speaker.title}</p>
       </div>
-    </div>
+    </a>
   );
 }
 
