b2fff0a51abb712c1194500f5fed50b364d3b593
Jhin Lee <leehack@gmail.com>
2024-09-13 11:06:58 -0400

    add anchors

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,22 @@ import Venue from '@/components/sections/Venue';
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-around gap-20">
-      <EventHeader eventData={eventHeader} />
-      <EventPhotos />
-      <Sponsors />
-      <Communities />
-      <Venue venueData={venue} />
+    <div className="flex flex-col justify-around gap-20" >
+      <section id="event" className="scroll-mt-20">
+        <EventHeader eventData={eventHeader} />
+      </section>
+      <section id="photos" className="scroll-mt-20">
+        <EventPhotos />
+      </section>
+      <section id="sponsors" className="scroll-mt-20">
+        <Sponsors />
+      </section>
+      <section id="communities" className="scroll-mt-20">
+        <Communities />
+      </section>
+      <section id="venue" className="scroll-mt-20">
+        <Venue venueData={venue} />
+      </section>
     </div>
   );
 }
