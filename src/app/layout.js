c7727cd3f23a4c688cf7f6f4f2fe5db32434473c
Jhin Lee <leehack@gmail.com>
2024-10-25 15:25:15 -0400

    add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,20 +11,24 @@ const openSans = Open_Sans({
   display: 'swap',
 });
 export const metadata = {
-  title: 'DevFest Montreal 2024 | GDG Montreal',
-  description: 'DevFest Montreal 2024 | Google Developers Group Montreal | GDG Cloud Montreal | WTM Montreal | Flutter Montreal | GDSC Concordia | GDSC McGill',
+  metadataBase: new URL('http://devfest.gdgmontreal.com/'),
+  title: 'DevFest Montreal 2024 | Google Developers Group Montreal | GDG Cloud Montreal | WTM Montreal | Flutter Montreal | GDSC Concordia | GDSC McGill',
+  description: 'DevFests are local tech conferences hosted by Google Developer Groups (GDG) around the world. Each DevFest event is crafted by its local organizers to fit the needs and interests of its local developer community. Whether it be through hands-on learning experiences, technical talks delivered in local languages by experts, or by simply meeting fellow local developers, DevFest attendees learn how to build together and innovate on Google’s developer tools.',
+  openGraph: {
+    images: ['/images/logo/eventHeader.gif'],
+  }
 };
 
 export default function RootLayout({ children }) {
   return (<html lang="en">
     <body className={openSans.className}>
-    <Header />
-    <main className="container flex mx-auto px-5 flex-col flex-grow"> {/* Allow main content to expand */}
-      {children}
-    </main>
-    <Footer />
-    <GoogleAnalytics />
+      <Header />
+      <main className="container flex mx-auto px-5 flex-col flex-grow"> {/* Allow main content to expand */}
+        {children}
+      </main>
+      <Footer />
+      <GoogleAnalytics />
     </body>
-    </html>
+  </html>
   );
 }
