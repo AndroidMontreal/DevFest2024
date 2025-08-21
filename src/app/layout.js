2524ad250df7a03fb0699b6722c8cd740ba38d37
Laurence de Villers <laurence.de.villers.prog@gmail.com>
2025-08-21 09:23:40 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ const openSans = Open_Sans({
   display: 'swap',
 });
 export const metadata = {
-  metadataBase: new URL('http://devfest.gdgmontreal.com/'),
+  metadataBase: new URL('http://devfest2024.gdgmontreal.com/'),
   title: 'DevFest Montreal 2024 | Google Developers Group Montreal | GDG Cloud Montreal | WTM Montreal | Flutter Montreal | GDSC Concordia | GDSC McGill',
   description: 'DevFests are local tech conferences hosted by Google Developer Groups (GDG) around the world. Each DevFest event is crafted by its local organizers to fit the needs and interests of its local developer community. Whether it be through hands-on learning experiences, technical talks delivered in local languages by experts, or by simply meeting fellow local developers, DevFest attendees learn how to build together and innovate on Google’s developer tools.',
   openGraph: {
